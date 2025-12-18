c49812d8c550a481829c97a31ad04c365d8e2c19
Marwa Khot <marwakhot007@gmail.com>
2025-12-18 20:28:34 +0400

    Update database password in db.js

--- pear_backend/db.js
+++ pear_backend/db.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'marwakhot17',
+  password: 'your_password_here',
   database: 'pear'
 });
 
